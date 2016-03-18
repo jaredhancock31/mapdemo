e729655dad61e0139529e803157cf4603c94d729
jared <jaredhancock31@gmail.com>
2016-03-17 19:20:45 -0500

    added AsyncGet class; currently same as AsyncPost, so it's not ready

--- MyApplication/app/src/main/java/edu/txstate/jared/api/AsyncGet.java
+++ MyApplication/app/src/main/java/edu/txstate/jared/api/AsyncGet.java
@@ -1,18 +1,51 @@
 package edu.txstate.jared.api;
 
 import android.os.AsyncTask;
+import android.util.Log;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 /**
  * Created by jared on 3/17/16.
  */
 public class AsyncGet extends AsyncTask<String, Void, Boolean> {
 
-
+    private String TAG = "ASYNCGET";
     // default constructor
     public AsyncGet() {}
 
     @Override
     protected Boolean doInBackground(String... params) {
-        return null;
+        try {
+            String paramString = params[0];
+            URL hostUrl = new URL("http://104.236.181.178");
+            HttpURLConnection conn = (HttpURLConnection) hostUrl.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setDoOutput(true);
+            OutputStream os = conn.getOutputStream();
+            os.write(paramString.getBytes());
+            os.flush();
+            os.close();
+
+            int responseCode = conn.getResponseCode();
+            Log.i(TAG, "Post response Code : " + responseCode);
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return false;
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
+
 }
